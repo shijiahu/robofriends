5eb1ec18c757f3383bda944826a09b459a31e404
Shijia Hu <shijiahu2016a@gmail.com>
2018-07-08 23:52:47 -0400

    first commit

---
+++ robofriends/src/CardList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots})=>{
+	const cardsArray = robots.map((user)=>{
+		return <Card id={user.id} name={user.name} email={user.email} />
+	})
+	return (
+		<div>
+			{cardsArray}
+		</div>
+	);
+}
+export default CardList;
