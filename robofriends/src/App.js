eb667c29f3fe3d8752b9e1e8a7d54028741d26ea
Shijia Hu <shijiahu2016a@gmail.com>
2018-07-09 15:20:07 -0400

    add fetch from jsonplaceholder

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CardList from './CardList';
 import SerchBox from './SerchBox';
-import { robots } from './robots';
 import './App.css';
 
 
@@ -9,13 +8,19 @@ class App extends React.Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
 	}
 
 	onSearchChange = (event) => {
 		this.setState({searchfield: event.target.value}) ;
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+  		.then(users => this.setState({robots: users}));
 		
 	}
 	render(){
