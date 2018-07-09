ca40047661da2c479a694763e7f1c9b2a97bcd58
Shijia Hu <shijiahu2016a@gmail.com>
2018-07-09 01:25:40 -0400

    add search function & title

---
+++ robofriends/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import CardList from './CardList';
+import SerchBox from './SerchBox';
+import { robots } from './robots';
+
+
+class App extends React.Component {
+	constructor(){
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value}) ;
+		
+	}
+	render(){
+		const filteredRobot = this.state.robots.filter(robots => {
+				return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+			}
+		)
+		return(
+			<div className='tc'>
+				<h1>RoboFriends</h1>
+				<SerchBox searchChange = {this.onSearchChange}/>
+				<CardList robots={filteredRobot} />
+			</div>
+		);
+	}
+}
+
+export default App;
--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 
-const Card = (props)=>{
-	const {name, email, id} = props;
+const Card = ({name, email, id})=>{
 	return (
 		<div className='bg-light-green dib br3 pa3 ma2 grow shadow-5'>
 			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
